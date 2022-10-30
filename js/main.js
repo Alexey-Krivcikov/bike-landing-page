559b82076bd65b1bec9a2da9a73fc6b117d7d9ab
Alexey Krivchikov <krivchikovalexei@yandex.ru>
2022-10-30 11:01:53 +0300

    make footer

--- js/main.js
+++ js/main.js
@@ -15,4 +15,18 @@ $(function(){
     autoplaySpeed: 2000
   });
 
+  $(".menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		let id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 300 мс
+		$('body,html').animate({scrollTop: top}, 300);
+	});
+
 });
